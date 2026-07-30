9a9b84a0a6ae849a2cd4d6572ee32f623f101b14
draven <dravengarden@gmail.com>
2026-07-30 17:42:43 +0800

    Replay navigation highlight after editor reconfigure

--- web/src/mobile/review/CodeViewer.tsx
+++ web/src/mobile/review/CodeViewer.tsx
@@ -715,7 +715,7 @@ export default function CodeViewer({
     view.dispatch({
       effects,
     });
-  }, [revealLine, revealRange, text]);
+  }, [extensions, revealLine, revealRange, text]);
 
   return (
     <Box
