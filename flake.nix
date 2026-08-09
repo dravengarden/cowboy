eca13af0be98d99597dd01c790238ae2a99582ad
draven <dravengarden@gmail.com>
2026-08-10 02:23:24 +0800

    fix: normalize web dependency cache

--- flake.nix
+++ flake.nix
@@ -133,7 +133,7 @@
         pname = "cowboy";
         version = "0.1.0";
         src = pkgs.lib.cleanSource ./.;
-        depsHash = "sha256-AE/dUVCnmp1nFOBR1YV6SDwvw9GIFuIA27qlfTioWEk=";
+        depsHash = "sha256-ySwkADTVLwf8Us8rpscTggCx+mvJj7z0YOBBR/QIyH4=";
       };
 
       # This host's pinned Nixpkgs still has the first fetchCargoVendor
