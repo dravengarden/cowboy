9c2f3371d5b1fa72fca69853ec68a4ddbdb65393
draven <dravengarden@gmail.com>
2026-08-30 13:27:26 +0800

    fix(nix): refresh Cowboy cargo vendor hash

--- flake.nix
+++ flake.nix
@@ -193,7 +193,7 @@
         pname = "cowboy";
         version = "0.1.0";
         src = cowboy-src;
-        hash = "sha256-+2B/wYD6IfKuwqr7zHHb/SpFXBBGx2rky1tcJeTyYdk=";
+        hash = "sha256-ct8KpXOAuIJZ4vQpMmwMuTeOzhdNLVmNWjNGa+HryJo=";
         preBuild = ''
           vendor_util="$(command -v fetch-cargo-vendor-util-v2 || command -v fetch-cargo-vendor-util)"
           if grep -q "https://crates.io/api/v1/crates/" "$vendor_util"; then
